2c004af33cca743d1fbe71413b51ba3dccfec2ee
AlmaznayaGroza <geraldinerassam@gmail.com>
2024-10-19 23:18:34 +0200

    Modif script ex.1 avec boucle FOR

--- Exercices/comptes.sh
+++ Exercices/comptes.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
 
-# Je compte pour 2016
+# J'utilise une boucle FOR pour itérer sur les 3 années
 
-lieux2016=$(cat ../../ann/2016/*.ann | grep Location | wc -l)
-echo "Lieux en 2016:"
-echo "$lieux2016"
-
-
-# Puis pour 2017
-
-lieux2017=$(cat ../../ann/2017/*.ann | grep Location | wc -l)
-echo -e "Lieux en 2017:\n$lieux2017"
-
-
-# Et enfin pour 2018
-
-lieux2018=$(cat ../../ann/2018/*.ann | grep Location | wc -l)
-echo -e "Lieux en 2018:\n$lieux2018"
-
-
+for ANNEE in 2016 2017 2018
+do
+	LIEUX=$(cat ../../ann/$ANNEE/*.ann | grep Location | wc -l)
+	echo -e "Lieux en $ANNEE:\n$LIEUX"
+done

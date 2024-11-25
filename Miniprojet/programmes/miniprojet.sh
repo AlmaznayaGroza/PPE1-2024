4007445ab5804b48eac051ebe31d8d808df9bc69
AlmaznayaGroza <geraldinerassam@gmail.com>
2024-11-25 18:38:00 +0100

    Update script tableau avec Bulma (ex.4)

--- Miniprojet/programmes/miniprojet.sh
+++ Miniprojet/programmes/miniprojet.sh
@@ -1,73 +1,54 @@
-#!/usr/bin/env bash
-
-if [ $# -ne 1 ]
-then
-	echo "Ce programme prend un argument!"
-	exit 1
-fi
+#!/bin/bash
 
 FICHIER=$1
-OUTPUT="../tableaux/tableau-fr.html"
+OUTPUT="../tableaux/tableau.html"
 
-# Écrire l'entête HTML
-cat <<EOF > $OUTPUT
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <title>Tableau URLs</title>
-    <meta charset="UTF-8">
-    <style>
-      table,
-      th,
-      td {
-        padding: 10px;
-        border: 1px solid black;
-        border-collapse: collapse;
-      }
-    </style>
-</head>
-<body>
-    <table>
-		<caption>Résultats de l'analyse des URLs</caption>
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>URL</th>
-                <th>Code HTTP</th>
-                <th>Encodage</th>
-                <th>Nombre de mots</th>
-            </tr>
-        </thead>
-        <tbody>
-EOF
+{
+echo "<html>
+	<head>
+		<meta charset=\"UTF-8\">
+	</head>
+
+	<body>
+		<table>
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>URL</th>
+					<th>Code HTTP</th>
+					<th>Encodage</th>
+					<th>Nombre de mots</th>
+				</tr>
+			</thead>
+			<tbody>"
 
 lineno=1
-while read -r line
+while read -r URL
 do
-	http_code=$(curl -s -I -L -w "%{http_code}" -o /dev/null $line)
-	encodage=$(curl -s -I -L -w "%{content_type}" -o /dev/null $line | egrep -o "charset=\S+" | cut -d= -f2)
+	reponse=$(curl -s -L -w "%{content_type}\t%{http_code}" -o /tmp/contenupages.html $URL)
+	http_code=$(echo "$reponse" | cut -f2)
+	content_type=$(echo "$reponse" | cut -f1)
+	encodage=$(echo "$content_type" | egrep -o "charset=\S+" | cut -d "=" -f2 | tail -n1)
+	# valeur par défaut si encodage est vide (car non trouvé)
 	encodage=${encodage:-"N/A"}
-	nb_mots=$(lynx -dump -nolist $line | wc -w)
-
-    # on ajoute dans OUTPUT
-	cat <<EOF >> $OUTPUT
-            <tr>
-                <td>${lineno}</td>
-                <td><a href="${line}">${line}</a></td>
-                <td>${http_code}</td>
-                <td>${encodage}</td>
-                <td>${nb_mots}</td>
-            </tr>
-EOF
+	nb_mots=$(lynx -dump -nolist /tmp/contenupages.html | wc -w | sed 's/^[ \t]*//;s/[ \t]*$//')
+	# sed supprime les éventuels tabulations ou espaces qui traînent en début et fin de ligne
+	
+	#echo -e "$lineno\t$URL\t$http_code\t$encodage\t$nb_mots"
+	
+	echo "<tr>
+		<td>$lineno</td>
+		<td>$URL</td>
+		<td>$http_code</td>
+		<td>$encodage</td>
+		<td>$nb_mots</td>
+	</tr>"
+	
     ((lineno++))
 done < "$FICHIER"
 
-# on finit en ajoutant les balises de fermeture
-cat <<EOF >> $OUTPUT
-        </tbody>
-    </table>
+echo "</tbody>
+</table>
 </body>
-</html>
-EOF
-
-echo "Fichier HTML généré: $OUTPUT"
+</html>"
+} > "$OUTPUT"
